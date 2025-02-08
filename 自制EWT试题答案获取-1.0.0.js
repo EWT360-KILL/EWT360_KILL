524a6f91259bd7a4529ea0654d4fe084c2cf70ba
EWT360-KILL <EWT360_KILL@outlook.com>
2025-02-08 23:39:29 +0800

    Update 1.0.1
    一点小更改

--- 自制EWT试题答案获取-1.0.0.js
+++ 自制EWT试题答案获取-1.0.0.js
@@ -1,14 +1,15 @@
 // ==UserScript==
-// @name         自制EWT试题答案获取
-// @name:en         EWT360_KILL
-// @namespace    https://github.com/EWT360-KILL/EWT360_KILL
-// @version      1.0.0
-// @description  自制EWT试题答案获取
-// @author       EWT360_KILL
-// @match        https://web.ewt360.com/mystudy/
-// @icon         https://web.ewt360.com/common/img/favicon.ico
-// @grant        none
-// @license         GPL-3.0-or-later
+// @name         	自制EWT试题答案获取
+// @name:en      	EWT360_KILL
+// @namespace    	https://github.com/EWT360-KILL/EWT360_KILL
+// @version      	1.0.0
+// @description  	自动获取升学e网通试题答案，包括但不限于课后习题、试卷答案、周培优。
+// @description:en	Automatically obtain the answers to the questions on the EWT360, including but not limited to after-class exercises, test paper answers, and Zhou Peiyou.
+// @author       	EWT360_KILL
+// @match        	https://web.ewt360.com/mystudy/
+// @icon         	https://web.ewt360.com/common/img/favicon.ico
+// @grant        	none
+// @license      	GPL-3.0-or-later
 // ==/UserScript==
 
 /**
